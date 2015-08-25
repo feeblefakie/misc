a01a13d55d822b5a02c564b7a3aa28c09b4e243f
Hiroyuki Yamada <hiroyuki@tkl.iis.u-tokyo.ac.jp>
2015-08-25 16:51:26 +0900

    added HttpStats to store stats information

--- golang/iobench/httpbench.go
+++ golang/iobench/httpbench.go
@@ -9,6 +9,13 @@ import (
 	"time"
 )
 
+type HttpStats struct {
+	doneRequests   int64
+	numSucceeded   int64
+	numFailed      int64
+	accumLatencies int64
+}
+
 func main() {
 
 	url := flag.String("url", "", "url")
@@ -22,34 +29,41 @@ func main() {
 	}
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	var doneRequests int64 = 0
+	stats := HttpStats{0, 0, 0, 0.0}
 
 	for i := 0; i < *concurrency; i++ {
-		go httpRequest(url, &doneRequests, *total)
+		go httpRequest(url, &stats, *total)
 	}
 
 	start := time.Now()
 	//var input string
 	//fmt.Scanln(&input)
-	for doneRequests < *total {
+	for stats.doneRequests < *total {
 		end := time.Now()
 		interval := int64(end.Sub(start).Seconds())
 		if interval != 0 {
-			fmt.Printf("total: %7d, reqs/s: %7d\r", doneRequests, doneRequests/interval)
+			fmt.Printf("ok: %6d,  errors: %6d,  reqs/s: %6d,  accumLat: %6d, aveLat(ms): %6d \r", stats.numSucceeded, stats.numFailed, stats.numSucceeded/interval, stats.accumLatencies, stats.accumLatencies/interval/1000000)
 		}
 		time.Sleep(time.Millisecond * 100)
 	}
 }
 
-func httpRequest(url *string, doneRequests *int64, total int64) {
+func httpRequest(url *string, stats *HttpStats, total int64) {
 	for {
-		atomic.AddInt64(doneRequests, 1)
+		atomic.AddInt64(&stats.doneRequests, 1)
 
 		req, _ := http.NewRequest("GET", *url, nil)
 		client := new(http.Client)
+		start := time.Now()
 		resp, err := client.Do(req)
+		end := time.Now()
+		interval := int64(end.Sub(start).Nanoseconds())
+		atomic.AddInt64(&stats.accumLatencies, interval)
 		if err != nil {
 			fmt.Println(err)
+			atomic.AddInt64(&stats.numFailed, 1)
+		} else {
+			atomic.AddInt64(&stats.numSucceeded, 1)
 		}
 		//fmt.Println(err)
 		//fmt.Println(resp)
