9aba4ea0c13e5de1c96085f622e83fddc149b930
Hiroyuki Yamada <mogwaing@gmail.com>
2015-08-30 14:41:29 +0900

    now accept a file which composed of urls to be accessed formatted like "Method URL BodyParameter"

--- golang/httpbench/httpbench.go
+++ golang/httpbench/httpbench.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 	"runtime"
+	"strconv"
+	"strings"
 	"sync/atomic"
 	"time"
 )
@@ -17,56 +22,86 @@ type HttpStats struct {
 }
 
 func main() {
-
-	url := flag.String("url", "", "url")
-	// random mode only for now
+	file := flag.String("file", "", "a file which has list of URLs to be accessed")
+	tmpfile := "/tmp/" + strconv.FormatInt(int64(os.Getegid()), 10) + ".err"
+	efile := flag.String("error_to", tmpfile, "a file for error messages")
 	concurrency := flag.Int("concurrency", 1, "concurrency")
 	total := flag.Int64("total", 10000, "total")
 	flag.Parse()
-	if *url == "" {
+
+	if *file == "" {
 		flag.Usage()
 		return
 	}
 
+	f, err := os.Open(*file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	ef, err := os.Create(*efile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer ef.Close()
+
+	scanner := bufio.NewScanner(f)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	stats := HttpStats{0, 0, 0, 0.0}
+	stats := &HttpStats{}
 
 	for i := 0; i < *concurrency; i++ {
-		go httpRequest(url, &stats, *total)
+		go httpRequest(lines, stats, *total, ef)
 	}
 
 	start := time.Now()
-	//var input string
-	//fmt.Scanln(&input)
 	for stats.doneRequests < *total {
 		end := time.Now()
 		interval := int64(end.Sub(start).Seconds())
 		if interval != 0 {
-			fmt.Printf("ok: %6d,  errors: %6d,  reqs/s: %6d,  accumLat: %6d, aveLat(ms): %6d \r", stats.numSucceeded, stats.numFailed, stats.numSucceeded/interval, stats.accumLatencies, stats.accumLatencies/interval/1000000)
+			fmt.Printf("ok: %6d,  errors: %6d,  reqs/s: %6d,  accumLat: %6d, aveLat(ms): %6d \r",
+				stats.numSucceeded, stats.numFailed, stats.numSucceeded/interval, stats.accumLatencies, stats.accumLatencies/interval/1000000)
 		}
 		time.Sleep(time.Millisecond * 100)
 	}
 }
 
-func httpRequest(url *string, stats *HttpStats, total int64) {
+func httpRequest(lines []string, stats *HttpStats, total int64, errorFile *os.File) {
 	for {
-		atomic.AddInt64(&stats.doneRequests, 1)
+		offset := atomic.AddInt64(&stats.doneRequests, 1)
+		if int(offset) > len(lines) {
+			break
+		}
+		line := lines[offset-1]
+		//fmt.Println(line)
 
-		req, _ := http.NewRequest("GET", *url, nil)
+		// format: [Method URL BodyParameters(for POST)]
+		items := strings.Split(line, " ")
+		var body io.Reader
+		if len(items) > 2 {
+			body = strings.NewReader(items[2])
+		}
+		req, _ := http.NewRequest(items[0], items[1], body)
 		client := new(http.Client)
 		start := time.Now()
 		resp, err := client.Do(req)
 		end := time.Now()
 		interval := int64(end.Sub(start).Nanoseconds())
+		//fmt.Println(resp)
 		atomic.AddInt64(&stats.accumLatencies, interval)
 		if err != nil {
-			fmt.Println(err)
 			atomic.AddInt64(&stats.numFailed, 1)
+			errorFile.WriteString(err.Error() + "\n")
 		} else {
 			atomic.AddInt64(&stats.numSucceeded, 1)
 		}
-		//fmt.Println(err)
-		//fmt.Println(resp)
 		resp.Body.Close()
 	}
 }
