4f4f123a2bc77476dfcc8bc6520baf50213974c9
Hiroyuki Yamada <hiroyuki@tkl.iis.u-tokyo.ac.jp>
2015-10-11 12:44:13 +0900

    updated

--- cassandra/GettingStarted.java
+++ cassandra/GettingStarted.java
@@ -1,5 +1,6 @@
 
 import com.datastax.driver.core.*;
+import java.util.Calendar;
 
 public class GettingStarted {
     //public static Logger logger = Logger.getLogger("RbReport");
@@ -10,38 +11,51 @@ public class GettingStarted {
 
         try {
 
+            long start = System.currentTimeMillis();
             // Connect to the cluster and keyspace "demo"
             cluster = Cluster.builder().addContactPoint("192.168.110.103").build();
             session = cluster.connect("mykeyspace");
+            long cp0 = System.currentTimeMillis();
+            System.out.println(cp0 - start);
 
             // Insert one record into the users table
             session.execute("INSERT INTO users (user_id, fname, lname, number) VALUES (1000, 'john', 'yamada', 100)");
-            System.out.println("hello1");
+            long cp1 = System.currentTimeMillis();
+            System.out.println(cp1 - cp0);
 
             // Use select to get the user we just entered
             ResultSet results = session.execute("SELECT * FROM users WHERE lname='yamada'");
             for (Row row : results) {
                 System.out.format("%d %s %s %d\n", row.getInt("user_id"), row.getString("fname"), row.getString("lname"), row.getInt("number"));
             }
-            System.out.println("hello2");
+            long cp2 = System.currentTimeMillis();
+            System.out.println(cp2 - cp1);
 
             // Update the same user with a new age
             session.execute("update users set number = 101 where user_id = 1000");
+            long cp3 = System.currentTimeMillis();
+            System.out.println(cp3 - cp2);
+
             // Select and show the change
             results = session.execute("select * from users where lname='yamada'");
             for (Row row : results) {
                 System.out.format("%d %s %s %d\n", row.getInt("user_id"), row.getString("fname"), row.getString("lname"), row.getInt("number"));
             }
-            System.out.println("hello3");
+            long cp4 = System.currentTimeMillis();
+            System.out.println(cp4 - cp3);
 
             // Delete the user from the users table
             session.execute("DELETE FROM users WHERE user_id = 1000");
+            long cp5 = System.currentTimeMillis();
+            System.out.println(cp5 - cp4);
+
             // Show that the user is gone
             results = session.execute("SELECT * FROM users");
             for (Row row : results) {
                 System.out.format("%d %s %s %d\n", row.getInt("user_id"), row.getString("fname"), row.getString("lname"), row.getInt("number"));
             }
-            System.out.println("hello4");
+            long cp6 = System.currentTimeMillis();
+            System.out.println(cp6 - cp5);
             /*
             */
 
