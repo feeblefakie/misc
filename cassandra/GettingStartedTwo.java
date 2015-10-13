52d8bd84a3000c88358fc4ea913aa38850eed5da
Hiroyuki Yamada <hiroyuki@tkl.iis.u-tokyo.ac.jp>
2015-10-13 14:54:29 +0900

    query builder with prepared statement

--- cassandra/GettingStartedTwo.java
+++ cassandra/GettingStartedTwo.java
@@ -26,15 +26,35 @@ public class GettingStartedTwo {
             System.out.println(cp0 - start);
 
             // Insert one record into the users table
+            /*
             PreparedStatement statement = session.prepare("INSERT INTO users (user_id, fname, lname, number) VALUES (?, ?, ?, ?)");
             BoundStatement boundStatement = new BoundStatement(statement);
             session.execute(boundStatement.bind(1000, "john", "yamada", 100));
+            */
+
+            /*
+            Statement statement = QueryBuilder.insertInto("mykeyspace", "users2")
+                .value("user_id", 100)
+                .value("fname", "john")
+                .value("lname", "yamada")
+                .value("number", 100);
+            session.execute(statement);
+            */
+
+            Insert insert = QueryBuilder.insertInto("mykeyspace", "users2")
+                .value("user_id", QueryBuilder.bindMarker())
+                .value("fname", QueryBuilder.bindMarker())
+                .value("lname", QueryBuilder.bindMarker())
+                .value("number", QueryBuilder.bindMarker());
+            PreparedStatement preparedStatement = session.prepare(insert);
+            BoundStatement boundStatement = new BoundStatement(preparedStatement);
+            session.execute(boundStatement.bind(100, "john", "yamada", 100));
 
             long cp1 = System.currentTimeMillis();
             System.out.println(cp1 - cp0);
 
             // Use select to get the user we just entered
-            Statement select = QueryBuilder.select().all().from("mykeyspace", "users")
+            Statement select = QueryBuilder.select().all().from("mykeyspace", "users2")
                 .where(QueryBuilder.eq("lname", "yamada"));
             results = session.execute(select);
             for (Row row : results) {
@@ -44,15 +64,15 @@ public class GettingStartedTwo {
             System.out.println(cp2 - cp1);
 
             // Update the same user with a new age
-            Statement update = QueryBuilder.update("mykeyspace", "users")
+            Statement update = QueryBuilder.update("mykeyspace", "users2")
                 .with(QueryBuilder.set("number", 101))
-                .where((QueryBuilder.eq("user_id", 1000)));
+                .where((QueryBuilder.eq("user_id", 100)));
             session.execute(update);
             long cp3 = System.currentTimeMillis();
             System.out.println(cp3 - cp2);
             
             // Select and show the change
-            select = QueryBuilder.select().all().from("mykeyspace", "users").where(QueryBuilder.eq("lname", "yamada"));
+            select = QueryBuilder.select().all().from("mykeyspace", "users2").where(QueryBuilder.eq("lname", "yamada"));
             results = session.execute(select);
             for (Row row : results) {
                 System.out.format("%d %s %s %d\n", row.getInt("user_id"), row.getString("fname"), row.getString("lname"), row.getInt("number"));
@@ -61,13 +81,13 @@ public class GettingStartedTwo {
             System.out.println(cp4 - cp3);
 
             // Delete the user from the users table
-            Statement delete = QueryBuilder.delete().from("users").where(QueryBuilder.eq("user_id", 1000));
+            Statement delete = QueryBuilder.delete().from("users2").where(QueryBuilder.eq("user_id", 100));
             results = session.execute(delete);
             long cp5 = System.currentTimeMillis();
             System.out.println(cp5 - cp4);
 
             // Show that the user is gone
-            select = QueryBuilder.select().all().from("mykeyspace", "users");
+            select = QueryBuilder.select().all().from("mykeyspace", "users2");
             results = session.execute(select);
             for (Row row : results) {
                 System.out.format("%d %s %s %d\n", row.getInt("user_id"), row.getString("fname"), row.getString("lname"), row.getInt("number"));
