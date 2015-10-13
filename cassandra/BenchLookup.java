665d32c57244d8fbb0ed11914ad190c8f954c5cc
Hiroyuki Yamada <hiroyuki@tkl.iis.u-tokyo.ac.jp>
2015-10-13 17:27:48 +0900

    multi-threaded

--- cassandra/BenchLookup.java
+++ cassandra/BenchLookup.java
@@ -1,15 +1,25 @@
 import java.io.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+ import java.util.concurrent.TimeUnit;
+
 import com.datastax.driver.core.*;
 import com.datastax.driver.core.querybuilder.*;
 import com.datastax.driver.core.policies.*;
 
 public class BenchLookup {
 
+    private static Cluster cluster;
+    private static Session session;
+    private static ResultSet results;
+    private static BlockingQueue<String> queue;
+    private static String endpoint = null;
+    private static String keyspace = null;
+    private static String table = null;
+    private static String filename = null;
+    private static int concurrency = 0;
+
     public static void main(String[] args) {
-        String endpoint = null;
-        String keyspace = null;
-        String table = null;
-        String filename = null;
 
         for (int i = 0; i < args.length; ++i) {
             if ("-endpoint".equals(args[i])) {
@@ -20,55 +30,57 @@ public class BenchLookup {
                 table = args[++i];
             } else if ("-filename".equals(args[i])) {
                 filename = args[++i];
+            } else if ("-concurrency".equals(args[i])) {
+                concurrency = Integer.parseInt(args[++i]);
             } else {
-                System.err.println("BenchInsert -endpoint hostname -keyspace keyspace -table table -filename access-list-file");
+                System.err.println("BenchLookup -endpoint hostname -keyspace keyspace -table table -filename access-list-file -concurrency concurrency");
             }
         }
         if (endpoint == null || keyspace == null || table == null || filename == null) {
-            System.err.println("BenchInsert -endpoint hostname -keyspace keyspace -table table -filename access-list-file");
+            System.err.println("BenchLookup -endpoint hostname -keyspace keyspace -table table -filename access-list-file -concurrency concurrency");
             System.exit(1);
         }
 
-        Cluster cluster;
-        Session session;
-        ResultSet results;
-        Row rows;
+        String[] endpoints = endpoint.split(":");
 
         try {
             cluster = Cluster
                 .builder()
-                .addContactPoint(endpoint)
+                .addContactPoints(endpoints)
                 .withRetryPolicy(DefaultRetryPolicy.INSTANCE)
                 .withLoadBalancingPolicy(new TokenAwarePolicy(new DCAwareRoundRobinPolicy()))
                 .build();
             session = cluster.connect(keyspace);
 
-            long start = System.currentTimeMillis();
-
+            queue = new LinkedBlockingQueue<String>();
             File file = new File(filename);
             BufferedReader br = new BufferedReader(new FileReader(file));
-
             int numRecords = 0;
             while (true) {
                 String line = br.readLine();
                 if (line == null) {
                     break;
                 }
-
-                String[] items = line.split("=");
-
-                Statement statement = QueryBuilder.select().all().from(table)
-                                            .where(QueryBuilder.eq(items[0], Integer.parseInt(items[1])));
-                results = session.execute(statement);
+                queue.add(line);
                 ++numRecords;
-                System.out.println("selected " + numRecords);
             }
             br.close();
 
+            long start = System.currentTimeMillis();
+            Thread []threads = new Lookuper[concurrency];
+            for (int i = 0; i < concurrency; ++i) {
+                threads[i] = new BenchLookup().new Lookuper();
+                threads[i].start();
+            }
+
+            for (int i = 0; i < concurrency; ++i) {
+                threads[i].join();
+            }
             long end = System.currentTimeMillis();
-            long interval = end - start / 1000;
-            System.out.println("time taken (ms) : " + interval);
-            System.out.println("throughput (records/s) : " + interval/numRecords/1000);
+
+            long interval = (end - start) / 1000;
+            System.out.println("time taken (s) : " + interval);
+            System.out.println("throughput (records/s) : " + numRecords/interval);
 
             // Clean up the connection by closing it
             cluster.close();
@@ -76,4 +88,28 @@ public class BenchLookup {
             e.printStackTrace();
         }
     }
+
+    private class Lookuper extends Thread {
+        public Lookuper() {
+        }
+
+        public void run() {
+            try {
+                while (true) {
+                    String line = queue.poll(1, TimeUnit.MILLISECONDS);
+                    if (line == null) {
+                        return;
+                    }
+                    String[] items = line.split("=");
+
+                    Statement statement = QueryBuilder.select().all().from(table)
+                                                .where(QueryBuilder.eq(items[0], Integer.parseInt(items[1])));
+                    results = session.execute(statement);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
