63d7298ceb3792fb70681aaa1dc6bfbe9b0442d6
Hiroyuki Yamada <hiroyuki@tkl.iis.u-tokyo.ac.jp>
2015-10-19 17:30:00 +0900

    added benchmark scripts

--- cassandra/fullscan-bench.sh
+++ cassandra/fullscan-bench.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-endpoints="aoba01:aoba02:aoba03:aoba04:aoba05:aoba06:aoba07:aoba08:aoba09:aoba10:aoba11:aoba12:aoba13:aoba14:aoba15:aoba16:aoba17"
+#endpoints="aoba01:aoba02:aoba03:aoba04:aoba05:aoba06:aoba07:aoba08:aoba09:aoba10:aoba11:aoba12:aoba13:aoba14:aoba15:aoba16:aoba17"
+endpoints="aoba01:aoba02:aoba03:aoba04"
 keyspace="mykeyspace"
-table="users2"
+table="users5"
 
 java -classpath "cassandra-java-driver-2.1.8/*:cassandra-java-driver-2.1.8/lib/*:/usr/share/cassandra/lib/*:." BenchFullscan -endpoint $endpoints -keyspace $keyspace -table $table
--- cassandra/select-bench.sh
+++ cassandra/select-bench.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 endpoints="aoba01:aoba02:aoba03:aoba04:aoba05:aoba06:aoba07:aoba08:aoba09:aoba10:aoba11:aoba12:aoba13:aoba14:aoba15:aoba16:aoba17"
+#endpoints="aoba01:aoba02:aoba03:aoba04"
 keyspace="mykeyspace"
-table="users2"
-filename="selectdata.dat.10000"
-concurrency=5
+table="users5"
+#filename="selectdata.dat.1000000.4nodes"
+filename="selectdata.dat.1000000"
+concurrency=100
 
 java -classpath "cassandra-java-driver-2.1.8/*:cassandra-java-driver-2.1.8/lib/*:/usr/share/cassandra/lib/*:." BenchLookup -endpoint $endpoints -keyspace $keyspace -table $table -filename $filename -concurrency $concurrency
