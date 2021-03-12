c5617ce5335b2033918cf9b22ad69ca6c8e93974
Hiroyuki Yamada <mogwaing@gmail.com>
2021-03-12 21:28:11 +0900

    add

--- jepsen/jepsen.etcdemo/src/jepsen/etcdemo.clj
+++ jepsen/jepsen.etcdemo/src/jepsen/etcdemo.clj
@@ -1,13 +1,84 @@
 (ns jepsen.etcdemo
-  (:require [jepsen.cli :as cli]
-            [jepsen.tests :as tests]))
+  (:require [clojure.tools.logging :refer :all]
+            [clojure.string :as str]
+            [jepsen [cli :as cli]
+             [control :as c]
+             [db :as db]
+             [tests :as tests]]
+            [jepsen.control.util :as cu]
+            [jepsen.os.debian :as debian]))
+
+(def dir "/opt/etcd")
+(def binary "etcd")
+(def logfile (str dir "/etcd.log"))
+(def pidfile (str dir "/etcd.pid"))
+
+(defn node-url
+  ""
+  [node port]
+  (str "http://" node ":" port))
+
+(defn peer-url
+  ""
+  [node]
+  (node-url node 2380))
+
+(defn client-url
+  ""
+  [node]
+  (node-url node 2379))
+
+(defn initial-cluster
+  ""
+  [test]
+  (->> (:nodes test)
+       (map (fn [node]
+              (str node "=" (peer-url node))))
+       (str/join ",")))
+
+(defn db
+  ""
+  [version]
+  (reify db/DB
+    (setup! [_ test node]
+      (info node "installing etcd" version "with" test)
+      (c/su
+       (let [url (str "https://storage.googleapis.com/etcd/" version
+                      "/etcd-" version "-linux-amd64.tar.gz")]
+         (cu/install-archive! url dir))
+
+       (cu/start-daemon!
+        {:logfile logfile
+         :pidfile pidfile
+         :chdir dir}
+        binary
+        :--log-output :stderr
+        :--name (name node)
+        :--listen-peer-urls (peer-url node)
+        :--advertise-client-urls (client-url node)
+        :--initial-cluster-state        :new
+        :--initial-advertise-peer-urls  (peer-url node)
+        :--initial-cluster              (initial-cluster test))
+       (Thread/sleep 10000)))
+
+    (teardown! [_ test node]
+      (info node "tearing down etcd")
+      (cu/stop-daemon! binary pidfile)
+      (c/su (c/exec :rm :-rf dir)))
+
+    db/LogFiles
+    (log-files [_ test node]
+      [logfile])))
 
 (defn etcd-test
   ""
   [opts]
   (merge tests/noop-test
-         {:pure-generators true}
-         opts))
+         opts
+         {:name "etcd"
+          :os debian/os
+          :db (db "v3.1.5")
+          :pure-generators true}))
 
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
